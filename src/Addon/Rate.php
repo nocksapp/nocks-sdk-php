040462e56d752156082d45910da5d4359210ccf5
Nocks <support@nocks.nl>
2016-11-29 21:46:51 +0100

    Update Rate.php
    Use http links as a quick fix for servers that not have updated OpenSSL (handshake failure)

--- src/Addon/Rate.php
+++ src/Addon/Rate.php
@@ -22,7 +22,7 @@ class Rate
         if(in_array($currencyCode, array('USD', 'GBP', 'EUR')))
         {
             // Powered by CoinDesk
-            $response = $this->client->get('https://api.coindesk.com/v1/bpi/currentprice.json');
+            $response = $this->client->get('http://api.coindesk.com/v1/bpi/currentprice.json');
             $response = json_decode($response->getBody()->getContents(), true);
 
             if(isset($response['bpi'][$currencyCode]['rate']))
@@ -33,7 +33,7 @@ class Rate
         elseif(in_array($currencyCode, array('BTC', 'NLG')))
         {
             // Powered by Bittrex
-            $response = $this->client->get('https://bittrex.com/api/v1.1/public/getticker?market=BTC-NLG');
+            $response = $this->client->get('http://bittrex.com/api/v1.1/public/getticker?market=BTC-NLG');
             $response = json_decode($response->getBody()->getContents(), true);
 
             if(isset($response['result']['Last']))
